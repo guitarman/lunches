c0301694c9c6ac62e34b0170849de9bd049173c1
Jan Toth <janototh@gmail.com>
2012-11-26 22:33:20 +0100

    today_foods and today_soups in restaurants model + index.html now shows real values

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,12 @@ class Restaurant < ActiveRecord::Base
   has_many :day_menus
   has_many :foods, :through => :day_menus
   has_many :soups, :through => :day_menus
+
+  def today_foods
+    self.foods.where("for_day < ? and for_day > ?", Time.now, 1.day.ago)
+  end
+
+  def today_soups
+    self.soups.where("for_day < ? and for_day > ?", Time.now, 1.day.ago)
+  end
 end
