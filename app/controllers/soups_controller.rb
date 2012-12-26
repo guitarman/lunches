cdcd669cfc50479d4bf8ac74c9537df12b006281
guitarman <janototh@gmail.com>
2012-12-26 14:58:14 +0100

    find_by_name for soups & foods, back action for their detail pages

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,7 +1,7 @@
 class SoupsController < ApplicationController
 
   def show
-    @soup = Soup.find(params[:id])
+    @soup = Soup.find_by_name(params[:id])
 
     respond_to do |format|
       format.html #show.html.erb
