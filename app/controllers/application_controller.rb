55677a81b0ca95411cf9662aa434ff234afff5a5
guitarman <janototh@gmail.com>
2012-12-26 14:36:29 +0100

    access denied shows alert instead of notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def access_denied
-    redirect_to login_path, :notice => "Pre pokračovanie musíš byť prihlásený" and return false
+    redirect_to login_path, :alert => "Pre pokračovanie musíš byť prihlásený" and return false
   end
 
   helper_method :current_user
