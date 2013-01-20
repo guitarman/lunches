84e544629f031cebc78c017fdc8fd91c70d3834e
guitarman <janototh@gmail.com>
2013-01-20 17:02:07 +0100

    sub_header with day name and date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# encoding: utf-8
 module ApplicationHelper
+  def day_name_and_date
+    days = %w(pondelok utorok streda štvrtok piatok sobota nedeľa)
+    days[DateTime.now.wday - 1] + ', ' + DateTime.now.strftime('%d. %m. %Y')
+  end
 end
