d2f082e08b1fe1fb2554768e03379eefc70eaf45
guitarman <janototh@gmail.com>
2013-05-12 17:52:35 +0200

    deleted unnecessary date, day name in application.html.erb + css & helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 # encoding: utf-8
 module ApplicationHelper
-  def day_name_and_date
-    days = %w(pondelok utorok streda štvrtok piatok sobota nedeľa)
-    days[DateTime.now.wday - 1] + ', ' + DateTime.now.strftime('%d. %m. %Y')
-  end
 end
